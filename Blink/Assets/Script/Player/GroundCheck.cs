13a8c97a29066c84a49f86a3abba3c22147b805d
seonhjeo <42938826+seonhjeo@users.noreply.github.com>
2022-03-14 15:23:34 +0900

    add slippery ground and slope

--- Blink/Assets/Script/Player/GroundCheck.cs
+++ Blink/Assets/Script/Player/GroundCheck.cs
@@ -7,7 +7,8 @@ public class GroundCheck : MonoBehaviour
     private bool checkBounceOption;
 
     public bool isGrounded;
-    public bool isSlided;
+    public bool isSlippered;
+    public bool isSloped;
 
     public void SetBoundOption(bool option)
     {
@@ -18,17 +19,47 @@ public class GroundCheck : MonoBehaviour
     {
         if (!isGrounded && collision.tag == "Ground" && !checkBounceOption)
             isGrounded = true;
+        else if (!isGrounded && collision.tag == "Slope")
+        {
+            isGrounded = true;
+            isSloped = true;
+        }
+        else if (!isGrounded && collision.tag == "Slipper")
+        {
+            isGrounded = true;
+            isSlippered = true;
+        }
     }
-    
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.tag == "Ground")
+        if (!isGrounded && collision.tag == "Ground" && !checkBounceOption)
             isGrounded = true;
+        else if (!isGrounded && collision.tag == "Slope")
+        {
+            isGrounded = true;
+            isSloped = true;
+        }
+        else if (!isGrounded && collision.tag == "Slipper")
+        {
+            isGrounded = true;
+            isSlippered = true;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "Ground")
             isGrounded = false;
+        else if (collision.tag == "Slope")
+        {
+            isGrounded = false;
+            isSloped = false;
+        }
+        else if (collision.tag == "Slipper")
+        {
+            isGrounded = false;
+            isSlippered = false;
+        }
     }
 }
--- Blink/Assets/Script/Player/PlayerController2.cs
+++ Blink/Assets/Script/Player/PlayerController2.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class PlayerController2 : MonoBehaviour
 {
-    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D myCol;
+    private Rigidbody2D myRigid;
     private GroundCheck myGround;
     private WallCheck myWall;
 
+    [HideInInspector]
+    public PhysicsMaterial2D groundPM;
+    [HideInInspector]
+    public PhysicsMaterial2D slipperPM;
+
     [Header("이동속도")]
-    public float speed = 6f;
+    public float speed = 4f;
     [Header("점프력")]
     public float jumpForce = 12f;
     [Header("버튼 누름에 따른 점프력 차이")]
@@ -18,7 +24,8 @@ public class PlayerController2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        myCol = GetComponent<BoxCollider2D>();
+        myRigid = GetComponent<Rigidbody2D>();
         myGround = transform.GetChild(0).GetComponent<GroundCheck>();
         myWall = transform.GetChild(1).GetComponent<WallCheck>();
     }
@@ -27,22 +34,37 @@ public class PlayerController2 : MonoBehaviour
     private void Update()
     {
         Move();
+        ChangeFriction();
+    }
+
+    private void ChangeFriction()
+    {
+        if (myGround.isSlippered)
+            myCol.sharedMaterial = slipperPM;
+        else
+            myCol.sharedMaterial = groundPM;
     }
 
     private void Move()
     {
-        float horizontal;
+        int horizontal;
+        horizontal = (int)Input.GetAxis("Horizontal");
 
-        horizontal = Input.GetAxis("Horizontal");
         DoWallJump(horizontal);
         if (myGround.isGrounded && Input.GetButtonDown("Jump"))
         {
-            rigidbody2d.AddForce(new Vector2(rigidbody2d.velocity.x, jumpForce), ForceMode2D.Impulse);
+            myRigid.AddForce(new Vector2(myRigid.velocity.x, jumpForce), ForceMode2D.Impulse);
         }
-        if (Input.GetButtonUp("Jump") && rigidbody2d.velocity.y > 0.0f)
-            rigidbody2d.velocity = (new Vector2(rigidbody2d.velocity.x, rigidbody2d.velocity.y * jumpDiff));
-        if (myGround.isGrounded)
-            rigidbody2d.velocity = new Vector2(horizontal * speed, rigidbody2d.velocity.y);
+        if (Input.GetButtonUp("Jump") && myRigid.velocity.y > 0.0f)
+            myRigid.velocity = (new Vector2(myRigid.velocity.x, myRigid.velocity.y * jumpDiff));
+        if (myGround.isGrounded && !myGround.isSloped)
+        {
+            myRigid.AddForce(new Vector2(horizontal * speed, 0f));
+            if (myRigid.velocity.x > speed)
+                myRigid.velocity = new Vector2(speed, myRigid.velocity.y);
+            else if (myRigid.velocity.x < -speed)
+                myRigid.velocity = new Vector2(-speed, myRigid.velocity.y);
+        }
     }
 
     private void DoWallJump(float horizontal)
@@ -50,9 +72,9 @@ public class PlayerController2 : MonoBehaviour
         if (myWall.hitWall && !myGround.isGrounded)
         {
             if (myWall.hitRightWall && horizontal < 0 && Input.GetButtonDown("Jump"))
-                rigidbody2d.AddForce(new Vector2(-speed * 2 / 3, jumpForce), ForceMode2D.Impulse);
+                myRigid.AddForce(new Vector2(-speed, jumpForce), ForceMode2D.Impulse);
             else if (myWall.hitLeftWall && horizontal > 0 && Input.GetButtonDown("Jump"))
-                rigidbody2d.AddForce(new Vector2(speed * 2 / 3, jumpForce), ForceMode2D.Impulse);
+                myRigid.AddForce(new Vector2(speed, jumpForce), ForceMode2D.Impulse);
         }
     }
 }
