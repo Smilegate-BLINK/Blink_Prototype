675aa8f25f1d4f226e2685d7dd8151b7ceecba1e
seonhjeo <42938826+seonhjeo@users.noreply.github.com>
2022-03-17 18:07:10 +0900

    make worldController to start timer

--- Blink/Assets/Script/World/GroundController.cs
+++ Blink/Assets/Script/World/GroundController.cs
@@ -10,44 +10,31 @@ public class GroundController : MonoBehaviour
     public Color eyeOpendColor = Color.black;
     public Color eyeClosedColor = Color.white;
 
-    private float startshadedTime;
-    private float shadedTimeTaken;
-    private float time;
-
     // Start is called before the first frame update
     void Start()
     {
         myWorld = GameObject.Find("World").GetComponent<WorldController>();
         mySprite = GetComponent<SpriteRenderer>();
-        time = 0f;
         eyeOpendColor.a = 1f;
         eyeClosedColor.a = 1f;
-        startshadedTime = myWorld.getStartshadedTime();
-        shadedTimeTaken = myWorld.getShadedTimeTaken();
-}
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (myWorld.doBlinkFunc)
+        Color tmp;
+
+        if (myWorld.getWorldBlackOut())
         {
-            // 눈이 감겨있으면
-            if (myWorld.getWorldBlackOut())
-            {
-                mySprite.color = eyeClosedColor;
-                time += Time.deltaTime;
-                if (time > shadedTimeTaken + startshadedTime)
-                    mySprite.color = eyeOpendColor;
-                else if (time > startshadedTime)
-                    mySprite.color = Color.Lerp(eyeClosedColor, eyeOpendColor, (time - startshadedTime) / shadedTimeTaken);
-            }
-            else
-            {
-                mySprite.color = Color.Lerp(eyeOpendColor, eyeClosedColor, time / 0.5f);
-                time = 0f;
-            }
+            tmp = eyeClosedColor;
+            tmp.a = myWorld.getWorldAlpha();
+            mySprite.color = tmp;
         }
         else
-            mySprite.color = eyeClosedColor;
+        {
+            tmp = eyeOpendColor;
+            tmp.a = 1f;
+            mySprite.color = tmp;
+        }
     }
 }
